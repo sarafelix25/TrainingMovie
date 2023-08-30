235898abae304097c6f8dc82b218f02336b60e0c
Sara Felix <sfelix@MacBook-Air-de-Sara.local>
2023-08-30 00:33:23 -0300

    apagando storyboard e configurando scene

---
+++ MovieTrending/MovieTrending/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  MovieTrending
+//
+//  Created by Sara Felix on 30/08/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        self.title = "Movie Trending"
+        view.backgroundColor = .white
+    }
+
+
+}
+
