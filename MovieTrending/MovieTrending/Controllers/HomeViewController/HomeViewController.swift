2d51261b9c8a576deed4c789c9ee2475d150eaf3
Sara Felix <sfelix@MacBook-Air-de-Sara.local>
2023-09-01 20:32:31 -0300

    create tableView structure and test snapshot

---
+++ MovieTrending/MovieTrending/Controllers/HomeViewController/HomeViewController.swift
@@ -0,0 +1,66 @@
+//
+//  ViewController.swift
+//  MovieTrending
+//
+//  Created by Sara Felix on 30/08/23.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+    
+    private lazy var titleView: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Movie Trending"
+        label.textColor = UIColor(named: "colorTitle")
+        label.font = .systemFont(ofSize: 24.0, weight: .bold)
+        return label
+    }()
+    
+    private lazy var tableView : UITableView = {
+        let tableView = UITableView(frame: .zero, style: .plain)
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.backgroundColor = UIColor(named: "colorBackground")
+        tableView.separatorStyle = .singleLine
+        tableView.delegate = self
+        tableView.dataSource = self
+        return tableView
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .clear
+        setLayout()
+    }
+    
+    private func setLayout() {
+        view.addSubview(titleView)
+        view.addSubview(tableView)
+        
+        NSLayoutConstraint.activate([
+            titleView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            titleView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            
+            tableView.topAnchor.constraint(equalTo: titleView.bottomAnchor, constant: 32.0),
+            tableView.leftAnchor.constraint(equalTo: view.leftAnchor),
+            tableView.rightAnchor.constraint(equalTo: view.rightAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+    }
+    
+}
+
+extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+//        cell.textLabel?.text = movies[indexPath.row].title
+//        cell.backgroundColor = .clear
+//        cell.textLabel?.textColor = .white
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+}
---
+++ MovieTrending/MovieTrendingTests/HomeViewControllerTests.swift
@@ -0,0 +1,18 @@
+//
+//  MovieTrendingTests.swift
+//  MovieTrendingTests
+//
+//  Created by Sara Felix on 01/09/23.
+//
+import SnapshotTesting
+import XCTest
+@testable import MovieTrending
+
+class HomeViewControllerTests: XCTestCase {
+
+    func testHomeViewController() {
+        let hc = HomeViewController()
+        
+        assertSnapshot(matching: hc, as: .image, record: true)
+    }
+}
